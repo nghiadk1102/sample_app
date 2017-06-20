219e036331602a039bfb3dcb8f4480f181d24d5b
nghiadk <nghiadk1102@gmail.com>
2017-06-20 14:13:10 +0700

    modeling-users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  before_save {email.downcase!}
+
+  validates :name, presence: true,
+    length: {maximum: Settings.model.users.maximum_name}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+    length: {maximum: Settings.model.users.maximum_email},
+    format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  validates :password, presence: true,
+    length: {minimum: Settings.model.users.minimum_password}
+
+  has_secure_password
 end
