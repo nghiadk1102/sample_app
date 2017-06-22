7a4f8377ff24f95ee8ed692959b90ae17029e234
nghiadk <nghiadk1102@gmail.com>
2017-06-22 17:48:46 +0700

    advenced-login

--- db/schema.rb
+++ db/schema.rb
@@ -9,8 +9,7 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 20170620070953) do
+ActiveRecord::Schema.define(version: 20170624031534) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +17,7 @@ ActiveRecord::Schema.define(version: 20170620070953) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
