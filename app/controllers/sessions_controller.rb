d168e6d3440018af61835163065fc45518330641
nghiadk <nghiadk1102@gmail.com>
2017-06-26 15:15:34 +0700

    edit and update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,8 @@ class SessionsController < ApplicationController
 
     if user && user.authenticate(params[:session][:password])
       log_in user
-      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
-      redirect_to user
+      "1" == params[:session][:remember_me] ? remember(user) : forget(user)
+      redirect_back_or user
     else
       flash.now[:danger] = t ".invalid"
       render :new
