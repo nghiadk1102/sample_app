0948b4c40d5e735446f01318134663c3884af532
nghiadk <nghiadk1102@gmail.com>
2017-07-03 10:33:22 +0700

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,6 +51,18 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
+  def following
+    @title = "Following"
+    @users = @user.following.paginate page: params[:page]
+    render "show_follow"
+  end
+
+  def followers
+    @title = "Followers"
+    @users = @user.followers.paginate page: params[:page]
+    render "show_follow"
+  end
+
   private
 
   def user_params
